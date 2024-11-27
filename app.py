ce2aae2e9d65f18d99527db43294242ab74b0cf7
farroshayray <farros.hr@gmail.com>
2024-11-27 09:39:45 +0700

    create initial blueprint

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # flask app.py
 from flask import Flask
-from flask import render_template
+from connectors.auth import auth as auth_blueprint
 
 app = Flask(__name__)
+
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 @app.route('/')
 def index():
