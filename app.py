efd5366f7772c2d06f0edcd3b4507b2d29744f9c
farroshayray <farros.hr@gmail.com>
2024-11-27 09:20:02 +0700

    create flask project

---
+++ app.py
@@ -0,0 +1,13 @@
+# flask app.py
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<div>hello </div>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
