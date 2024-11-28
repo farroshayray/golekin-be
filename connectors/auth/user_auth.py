20a89e4d6255b3d6907c389bc558b6037d7a82cd
farroshayray <farros.hr@gmail.com>
2024-11-28 21:14:06 +0700

    add connectors

--- connectors/auth/user_auth.py
+++ connectors/auth/user_auth.py
@@ -1,6 +1,26 @@
 from . import auth
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.users import db, User
 
-@auth.route('/', methods=['GET'])
-def testauth():
-    return '<div>hello auth </div>'
+@auth.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username', '').strip()
+    fullname = data.get('fullname', '').strip()
+    email = data.get('email', '').strip()
+    password = data.get('password', '').strip()
+    pin = data.get('pin', '').strip()
+    role = data.get('role', '').strip()
+    phone_number = data.get('phone_number', '').strip()
+    agen_id_value = data.get('agen_id', '').strip()  # Get and strip the value
+    agen_id = int(agen_id_value) if agen_id_value else None
+    location = data.get('location', '').strip()
+
+    if not username or not fullname or not email or not password or not pin or not role or not phone_number:
+        return jsonify({'error': 'Please fill all fields'}), 400
+
+    user = User(username=username, fullname=fullname, email=email, password_hash=password, pin_hash=pin, role=role, phone_number=phone_number, agen_id=agen_id, location=location)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
